27c67caaa3a98404ec8128ed1c348737eee29690
Mechatrobot <90732298+Mechatrobot@users.noreply.github.com>
2024-07-11 11:56:49 +0100

    Update auto.py

--- file_test/auto.py
+++ file_test/auto.py
@@ -26,5 +26,5 @@ for commit in commits :
                        'Message' : commit.message.strip()
                       })
 data_frame = pd.DataFrame(commit_data) #convert commit data to DataFrame to put it on excel
-excel_file = os.path.join(local_file,'commits_list.xlsx')  # create the excel file
+excel_file = os.path.join('C:/Users/se93297/Desktop/TMP102-Driver-main/file_test','commits_list.xlsx')  # create the excel file
 data_frame.to_excel(excel_file, index=False, sheet_name= 'Commits') #assign DataFrame to excel
