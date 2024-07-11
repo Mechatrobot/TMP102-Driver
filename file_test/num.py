ebb868bfdf21645a0fa5ea20711e0a6ad6caeacd
Mechatrobot <90732298+Mechatrobot@users.noreply.github.com>
2024-07-11 12:11:53 +0100

    Update num.py

--- file_test/num.py
+++ file_test/num.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as np
 import opencv as cv
 import math,ll,
-print('salam') //test test t(g
+print('salam') //test test t(gvtrvr
 gergettt't"'t
 freff''thbyt
 brgbrccsdfvfdsd"gfzdede
